d47836cb47a9e4143240a413f2cfcd84ea20f093
Eliana Holanda <eliana.holanda85@gmail.com>
2022-08-01 16:25:50 -0300

    Teste com DoubleArray Operações

--- Dev_DigitalInovation_Kotlin/Collections/api-colletions/src/test/kotlin/onedigitatalinovation/collections/TesteOperações.kt
+++ Dev_DigitalInovation_Kotlin/Collections/api-colletions/src/test/kotlin/onedigitatalinovation/collections/TesteOperações.kt
@@ -14,4 +14,15 @@ fun main() {
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     println("----------")
     salariosMaiorQue2500.forEach { println(it) }
+
+    println("----------")
+    println(salarios.count { it in 2000.0 .. 5000.0})
+
+    println("----------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 3000.0 })
+
+    println("----------")
+    println(salarios.any { it == 2250.0 })
+    println(salarios.any { it == 4000.0 })
 }
